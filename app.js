d97054ced1bfbed91a8924fa81e018f7b7766f84
betterpepsis <bafykens@gmail.com>
2025-04-19 08:05:55 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Pokazywanie menu
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  document.querySelector('.menu').style.display = 'block';
+});
+
+// Tutaj będą dodawane funkcje ładowania opinii itp.
