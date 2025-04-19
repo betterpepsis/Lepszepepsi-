edf310c60bdce0f362834e22f2a1de1f89d4e2a5
betterpepsis <bafykens@gmail.com>
2025-04-19 08:10:10 +0200

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// Pokazywanie menu
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-  document.querySelector('.menu').style.display = 'block';
-});
-
-// Tutaj będą dodawane funkcje ładowania opinii itp.
